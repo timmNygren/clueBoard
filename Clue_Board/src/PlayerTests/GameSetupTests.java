f77e7d3eabb68e9198b97f907efdaa1c6c401cb5
Timm Nygren <tnygren@mines.edu>
2012-10-25 16:18:15 -0600

    Adding game setup tests and game action tests

---
+++ Clue_Board/src/PlayerTests/GameSetupTests.java
@@ -0,0 +1,36 @@
+package PlayerTests;
+import clueGame.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameSetupTests {
+	//Declarations/variables
+	public static Board board;
+	
+	
+	
+	@BeforeClass
+	public static void setUp() {
+		//stub
+	}
+	
+	
+	@Test
+	public void loadPeopleTest() {
+		//stub
+	}
+	
+	@Test
+	public void loadCardsTest() {
+		//stub
+	}
+	
+	@Test
+	public void dealCardsTest() {
+		//stub
+	}
+
+}
