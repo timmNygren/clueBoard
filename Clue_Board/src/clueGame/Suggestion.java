92770b8867a9f93c293f67ac8f4a19dba8272555
Timm Nygren <tnygren@mines.edu>
2012-10-26 20:08:38 -0600

    Finished failing tests

---
+++ Clue_Board/src/clueGame/Suggestion.java
@@ -0,0 +1,14 @@
+package clueGame;
+
+public class Suggestion {
+	public Card person;
+	public Card weapon;
+	public Card room;
+	
+	public Suggestion(Card person, Card weapon, Card room) {
+		this.person = person;
+		this.weapon = weapon;
+		this.room = room;
+	}
+	
+}
